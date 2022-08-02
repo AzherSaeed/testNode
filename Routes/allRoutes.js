e5f877cbeb3543fe605ade4e0f6eecd661cda141
AzherSaeed <chazher1997@gmail.com>
2022-08-02 20:33:37 +0500

    inintial

---
+++ Routes/allRoutes.js
@@ -0,0 +1,100 @@
+const express = require("express");
+const routers = express.Router();
+const multer = require("multer");
+const path = require("path");
+const fs = require("fs");
+const libre = require('libreoffice-convert');
+
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+        cb(null, "./uploads/");
+    },
+    filename: function (req, file, cb) {
+        cb(
+            null,
+            new Date().toISOString().replace(/:/g, "-") + "-" + file.originalname
+        );
+    },
+});
+
+
+
+const docsToPDF = (req , file , callback) => {
+    let ext = path.extname(file.originalname)
+
+
+    console.log(ext )
+
+    if(ext !== '.docx' && ext !== '.doc' ){
+        return callback('this is not supported')
+    }
+
+    callback(null , true)
+}
+
+
+
+const docxToPDF = multer({
+    storage: storage,
+    fileFilter : docsToPDF
+});
+
+routers.post('/down', docxToPDF.single('file') ,(req, res)=>{
+    console.log(req.file)
+
+
+    const file =  fs.readFileSync(req.file.path)
+    // let outputFilePath = Date.now() + "output.pdf"
+
+
+    res.status(200).json({
+        message : 'ok ha g',
+        outputFilePath : 'outputFilePath',
+        file : file
+    })
+
+})
+
+
+
+routers.post("/fileUpload",  docxToPDF.single('file') ,(req , res) => {
+    if(req.file){
+        const file =  fs.readFileSync(req.file.path)
+        let outputFilePath = Date.now() + "output.pdf"
+
+        libre.convert(file , '.pdf' , undefined , (err , done) => {
+            if(err){
+                fs.unlinkSync(req.file.path)
+                fs.unlinkSync(outputFilePath)
+
+                res.send('some error has taken in convertion')
+            }
+
+            try {
+                fs.writeFileSync(`./uploads/${outputFilePath}`, done)
+                console.log({outputFilePath})
+
+            }catch(err){
+                console.log(({err}))
+            }
+            res.download(`./uploads/${outputFilePath}` , (err , done) => {
+                if(err){
+                    console.log({err})
+                    fs.unlinkSync(req.file.path)
+                    fs.unlinkSync(outputFilePath)
+
+                    res.send('some error has taken in download ')
+                }
+                fs.unlinkSync(req.file.path)
+                fs.unlinkSync(`./uploads/${outputFilePath}`)
+
+            })
+        } )
+
+    }
+
+
+
+});
+
+module.exports = routers
